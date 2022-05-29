a92963903ea7ee0f61464efbe4b2d5cf05bdd965
Martin Larralde <martin.larralde@embl.de>
2022-05-29 15:57:02 +0200

    Add Cython extension declaring a wrapper for `SimilarityMatrix`

---
+++ pytrimal/__init__.py
@@ -0,0 +1,12 @@
+from ._version import __version__
+
+from . import _trimal
+from ._trimal import (
+    SimilarityMatrix
+)
+
+__doc__ = _trimal.__doc__
+__all__ = ["SimilarityMatrix"]
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
---
+++ pytrimal/_trimal.pyx
@@ -0,0 +1,49 @@
+# distutils: language = c++
+# cython: language_level=3, linetrace=True
+
+from libcpp cimport bool
+
+cimport trimal
+cimport trimal.report_system
+from trimal.similarity_matrix cimport similarityMatrix
+
+# --- Error management -------------------------------------------------------
+
+
+cdef class SimilarityMatrix:
+
+    cdef similarityMatrix _smx
+
+    @classmethod
+    def aa(cls):
+        cdef SimilarityMatrix matrix = cls.__new__(cls)
+        with nogil:
+            matrix._smx.defaultAASimMatrix()
+        return matrix
+
+    @classmethod
+    def nt(cls, bool degenerated=False):
+        cdef SimilarityMatrix matrix = cls.__new__(cls)
+        with nogil:
+            if degenerated:
+                matrix._smx.defaultNTSimMatrix()
+            else:
+                matrix._smx.defaultNTDegeneratedSimMatrix()
+        return matrix
+
+    def __cinit__(self):
+        self._smx
+
+    def __init__(self):
+        raise NotImplementedError("SimilarityMatrix.__init__")
+
+    cpdef float distance(self, str a, str b):
+        """distance(self, a, b)\n--
+
+        Return the distance between two sequence characters.
+
+        """
+        cdef float distance
+        # with nogil:
+        distance  = self._smx.getDistance(ord(a), ord(b))
+        return distance
