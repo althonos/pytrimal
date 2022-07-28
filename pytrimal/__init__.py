bcf4545cb366fe6c51aad80b86820def7d854267
Martin Larralde <martin.larralde@embl.de>
2022-07-28 16:33:54 +0200

    Add link to the documentation website in the package docstring [ci skip]

--- pytrimal/__init__.py
+++ pytrimal/__init__.py
@@ -27,3 +27,16 @@ __all__ = [
 
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "GPLv3"
+
+# Small addition to the docstring: we want to show a link redirecting to the
+# rendered version of the documentation, but this can only work when Python
+# is running with docstrings enabled
+if __doc__ is not None:
+    __doc__ += """See Also:
+    An online rendered version of the documentation for this version
+    of the library on
+    `Read The Docs <https://pytrimal.readthedocs.io/en/v{}/>`_.
+
+    """.format(
+        __version__
+    )
