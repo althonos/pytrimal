7cd47a6a1713640009eb572d6b07436897827c3f
Martin Larralde <martin.larralde@embl.de>
2022-08-13 17:57:43 +0200

    Rewrite benchmark code to show timings for the 3 main trimAl statistics

--- bench/bench.py
+++ bench/bench.py
@@ -6,14 +6,19 @@ import time
 import statistics
 import sys
 import warnings
+import typing
 from itertools import islice
+from typing import Dict, Callable, List
 
 sys.path.insert(0, os.path.realpath(os.path.join(__file__, "..", "..")))
 
 import numpy
 import pandas
 import rich.progress
-from pytrimal import Alignment, AutomaticTrimmer
+from pytrimal import Alignment, BaseTrimmer, AutomaticTrimmer, ManualTrimmer, OverlapTrimmer
+
+if typing.TYPE_CHECKING:
+    from pytrimal._trimal import TRIMMER_BACKEND
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--runs", default=3, type=int)
@@ -21,42 +26,58 @@ parser.add_argument("-d", "--data", required=True)
 parser.add_argument("-o", "--output", required=True)
 args = parser.parse_args()
 
+STATISTIC: Dict[ str, Callable[["TRIMMER_BACKEND"], BaseTrimmer] ] = {
+    "Gaps": lambda backend: ManualTrimmer(gap_threshold=0.5,backend=backend),
+    "Similarity": lambda backend: ManualTrimmer(similarity_threshold=0.5, backend=backend),
+    "Overlap": lambda backend: OverlapTrimmer(sequence_overlap=60, residue_overlap=0.5, backend=backend)
+}
+
 with rich.progress.Progress(transient=True) as progress:
     warnings.showwarning = lambda msg, c, f, l, file=None, line=None: progress.print(msg)
 
-    results = dict(results=[])
+    results: Dict[str, List[Dict[str, object]]] = dict(results=[])
     examples = glob.glob(os.path.join(args.data, "example.*.AA.*.fasta"))
 
     example = Alignment.load(os.path.join(args.data, "example.014.AA.EggNOG.COG0591.fasta"))
     subsets = numpy.linspace(10, len(example.sequences), 10, dtype=numpy.int32)
 
-    task = progress.add_task(total=len(subsets), description="Working...")
-    for subset in progress.track(subsets, task_id=task):
-        alignment = Alignment(example.names[:subset], example.sequences[:subset])
-        task2 = progress.add_task(total=2, description="Backends...")
-        for backend in progress.track(("sse", "generic", None), task_id=task2):
-            times = []
-            task3 = progress.add_task(total=args.runs, description="Repeat...")
-            for run in progress.track(range(args.runs), task_id=task3):
-                trimmer = AutomaticTrimmer(backend=backend, method="strict")
-                t1 = time.time()
-                trimmed = trimmer.trim(alignment)
-                t2 = time.time()
-                times.append(t2 - t1)
-            progress.remove_task(task3)
-            results["results"].append({
-                "backend": backend,
-                "residues": len(alignment.residues),
-                "sequences": len(alignment.sequences),
-                "times": times,
-                "mean": statistics.mean(times),
-                "stddev": statistics.stdev(times),
-                "median": statistics.median(times),
-                "min": min(times),
-                "max": max(times),
-            })
-        progress.remove_task(task_id=task2)
-    progress.remove_task(task_id=task)
+    task0 = progress.add_task(total=len(STATISTIC), description="Statistic (...)")
+    for statistic, get_trimmer in progress.track(STATISTIC.items(), task_id=task0):
+        progress.update(task_id=task0, description=f"Statistic ({statistic})")
+        backends: List["TRIMMER_BACKEND"] = ["sse", "generic", None]
+        task1 = progress.add_task(total=len(backends), description=" Backend (...)")
+        for backend in progress.track(backends, task_id=task1):
+            progress.update(task_id=task1, description=f" Backend ({backend})")
+            trimmer = get_trimmer(backend)
+            task2 = progress.add_task(total=len(subsets), description=f"  Subset (0/{len(example.sequences)})")
+            for subset in progress.track(subsets, task_id=task2):
+                progress.update(task_id=task2, description=f"  Subset ({subset}/{len(example.sequences)})")
+                times = []
+                alignment = Alignment(example.names[:subset], example.sequences[:subset])
+                task3 = progress.add_task(total=args.runs, description=f"  Repeat (0/{args.runs})")
+                for run in progress.track(range(args.runs), task_id=task3):
+                    progress.update(task_id=task3, description=f"  Repeat ({run}/{args.runs})")
+                    t1 = time.time()
+                    trimmed = trimmer.trim(alignment)
+                    t2 = time.time()
+                    times.append(t2 - t1)
+                progress.remove_task(task3)
+                results["results"].append({
+                    "backend": backend,
+                    "residues": len(alignment.residues),
+                    "sequences": len(alignment.sequences),
+                    "times": times,
+                    "mean": statistics.mean(times),
+                    "stddev": statistics.stdev(times),
+                    "median": statistics.median(times),
+                    "min": min(times),
+                    "max": max(times),
+                    "statistic": statistic,
+                    "trimmer": repr(trimmer),
+                })
+            progress.remove_task(task_id=task2)
+        progress.remove_task(task_id=task1)
+    progress.remove_task(task_id=task0)
 
 with open(args.output, "w") as f:
     json.dump(results, f, sort_keys=True, indent=4)
--- bench/plot.py
+++ bench/plot.py
@@ -29,47 +29,37 @@ for result in data["results"]:
     else:
         result["backend"] = result["backend"].upper()
 
-plt.figure(1, figsize=(6, 6))
+plt.figure(1, figsize=(18, 6))
 
-plt.subplot(1, 1, 1)
-data["results"].sort(key=lambda r: (r["backend"], r["sequences"]))
-for color, (backend, group) in zip(
-    Bold_4.hex_colors, itertools.groupby(data["results"], key=lambda r: r["backend"])
-):
-    group = list(group)
-    X = numpy.array([r["sequences"] for r in group])
-    Y = numpy.array([r["mean"] for r in group])
+data["results"].sort(key=lambda r: r["statistic"])
+for i, (statistic, statistic_group) in enumerate(itertools.groupby(data["results"], key=lambda r: r["statistic"])):
+    plt.subplot(1, 3, i+1)
+    plt.title(statistic)
 
-    p = Polynomial.fit(X, Y, 2)
-    pX = numpy.linspace(0, max(r["sequences"] for r in group), 1000)
-    # reg = scipy.stats.linregress(X, Y)
-    # plt.plot([ 0, max(X) ], [ reg.intercept, reg.slope*max(X) + reg.intercept ], color=color, linestyle="--", marker="")
-    # ci = [1.96 * r["stddev"] / math.sqrt(len(r["times"])) for r in group]
-    plt.scatter(X, Y, marker="+", color=color, label=f"{backend}")
-    plt.plot(pX, p(pX), color=color, linestyle="--")
+    statistic_group = sorted(statistic_group, key=lambda r: (r["backend"], r["sequences"]))
+    for color, (backend, group) in zip(
+        Bold_4.hex_colors, itertools.groupby(statistic_group, key=lambda r: r["backend"])
+    ):
+        group = list(group)
+        X = numpy.array([r["sequences"] for r in group])
+        Y = numpy.array([r["mean"] for r in group])
 
-plt.legend()
-plt.xlabel("Number of sequences")
-plt.ylabel("Time (s)")
+        if statistic == "Gaps":
+            reg = scipy.stats.linregress(X, Y)
+            plt.scatter(X, Y, marker="+", color=color, label=f"{backend} (R²={reg.rvalue**2:.3f})")
+            plt.plot([ 0, max(X) ], [ reg.intercept, reg.slope*max(X) + reg.intercept ], color=color, linestyle="--", marker="")
+        else:
+            p = Polynomial.fit(X, Y, 2)
+            pX = numpy.linspace(0, max(r["sequences"] for r in group), 1000)
+            # reg = scipy.stats.linregress(X, Y)
+            # plt.plot([ 0, max(X) ], [ reg.intercept, reg.slope*max(X) + reg.intercept ], color=color, linestyle="--", marker="")
+            # ci = [1.96 * r["stddev"] / math.sqrt(len(r["times"])) for r in group]
+            plt.scatter(X, Y, marker="+", color=color, label=f"{backend}")
+            plt.plot(pX, p(pX), color=color, linestyle="--")
 
-# plt.subplot(1, 2, 2)
-# data["results"].sort(key=lambda r: (r["backend"], r["residues"]))
-# for color, (backend, group) in zip(
-#     Bold_4.hex_colors, itertools.groupby(data["results"], key=lambda r: r["backend"])
-# ):
-#     group = list(group)
-#     X = numpy.array([r["residues"] for r in group])
-#     Y = numpy.array([r["mean"] for r in group])
-#     p = Polynomial.fit(X, Y, 2)
-#     # reg = scipy.stats.linregress(X, Y)
-#     # plt.plot([ 0, max(X) ], [ reg.intercept, reg.slope*max(X) + reg.intercept ], color=color, linestyle="--", marker="")
-#     # ci = [1.96 * r["stddev"] / math.sqrt(len(r["times"])) for r in group]
-#     plt.scatter(X, Y, marker="+", color=color, label=f"{backend}")
-#     plt.plot(X, p(X), color=color, linestyle="--")
-#
-# plt.legend()
-# plt.xlabel("Number of residues")
-# plt.ylabel("Time (s)")
+    plt.legend()
+    plt.xlabel("Number of sequences")
+    plt.ylabel("Time (s)")
 
 plt.tight_layout()
 output = args.output or args.input.replace(".json", ".svg")
